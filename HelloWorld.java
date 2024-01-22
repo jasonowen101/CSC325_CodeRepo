42696a214dc78b4c8947d4be836f92571c0442d9
Jason Owen <jasonowen101@gmail.com>
2024-01-22 14:23:53 -0600

    Hello World and ObjectExamples JTO

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-class HelloCSC325 {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to CSC325");
     }
---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a Dog instance
+        Dog myDog = new Dog("Buddy");
+
+        // Call methods for the Dog instance
+        myDog.makeSound();
+        myDog.move();
+
+        // Create a Cat instance
+        Cat myCat = new Cat("Whiskers");
+
+        // Call methods for the Cat instance
+        myCat.makeSound();
+        myCat.move();
+    }
+}
