ac57092b273cc2736303b4d3dca5dd81763ddc27
Jason Owen <jasonowen101@gmail.com>
2024-01-25 11:01:13 -0600

    JTO 1/25/24 Add frog boilerplate

---
+++ Frog.java
@@ -0,0 +1,7 @@
+public class Frog {
+    // Constructor for Frog class, calling the superclass constructor
+
+   // Implementation of the makeSound method for a Frog
+
+   // Implementation of the move method for a Frog
+}
