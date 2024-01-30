354737fc89b9d2109a06f88b43eb25be2b586800
Jason Owen <jasonowen101@gmail.com>
2024-01-30 08:01:56 -0600

    JTO 1/30/24 SimpleStartupCode

---
+++ StartupGame/SimpleStartup.java
@@ -0,0 +1,25 @@
+package StartupGame;
+public class SimpleStartup {
+    private int[] locationCells;
+    private int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+
+    public String checkYourself(int guess) {
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            } // end if
+        } // end for
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        } // end if
+        System.out.println(result);
+        return result;
+    } // end method
+} // close class
